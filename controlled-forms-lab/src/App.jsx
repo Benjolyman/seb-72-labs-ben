65036165b2a3047c8ec8df2e220cb38dccd79f86
benjolyman <benjolyman@hotmail.com>
2024-07-03 21:38:37 +1000

    completed

---
+++ controlled-forms-lab/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import { useState } from 'react';
+import Bookshelf from './components/Bookshelf';
+
+
+const App = () => {
+  return (
+    <>
+    <h1>My Bookshelf</h1>
+    <Bookshelf/>    
+    </>
+  )
+};
+
+export default App;
